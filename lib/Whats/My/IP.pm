41bd3da4abe4922039ebd1014d6d3b1fc05ba299
Hernan Lopes <hernan@cpan.org>
2014-07-20 21:00:53 -0300

    minor fix

--- lib/Whats/My/IP.pm
+++ lib/Whats/My/IP.pm
@@ -15,9 +15,8 @@ has ua => (
 
 sub get {
     my ( $self ) = @_; 
-    my $res         = $self->ua->get( "http://checkip.dyndns.org" );
-    my $ip          = $res->{ content };
-    ( $ip ) =  $ip  =~ m#.+body>Current IP Address: (.+)</body.+#g;
+    my $res    = $self->ua->get( "http://checkip.dyndns.org" );
+    my ( $ip ) = $res->{ content } =~ m#.+body>Current IP Address: (.+)</body.+#g;
     return 0 if ! $ip || $ip eq "";
     return $ip;
 }
