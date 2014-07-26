363513037e78e5d5b9606b0495a71b71768d4520
Hernan Lopes <hernan@cpan.org>
2014-07-26 15:08:04 -0300

    updated pod

--- lib/My/IP.pm
+++ lib/My/IP.pm
@@ -22,3 +22,35 @@ sub get {
 }
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+My::IP - $ myip prints your public ip address (checkip.dyndns.org)
+
+=head1 SYNOPSIS
+
+  use My::IP;
+
+=head1 DESCRIPTION
+
+My::IP prints your public ip address (checkip.dyndns.org)
+
+=head1 AUTHOR
+
+Hernan Lopes E<lt>hernan@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Hernan Lopes
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
